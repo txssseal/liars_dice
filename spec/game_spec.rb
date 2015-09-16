4446e101703d4c7a099a41e4aed9f0822f7d3145
Colton Seal <txssseal@users.noreply.github.com>
2015-09-16 12:15:39 -0500

    Update game_spec.rb

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -16,7 +16,7 @@ describe Game do
     expect(g.board).to eq([])
   end
 
-  describe "move()" do
+  describe "move" do
     it "moves a players dice onto the board, and subtracts the players dice count, then rerolls" do
       g.move(player: 2, dice: 1, value: 3)
       expect(g.players[1][:dice_count]).to eq(4)
@@ -25,14 +25,14 @@ describe Game do
     end
   end
 
-  describe "claim()" do
+  describe "claim" do
     it "has a correct probablity" do
       d = g.claim(dice: 5, value: 3)
       expect(d).to eq("11.646926278109309%")
     end
   end
 
-  describe "challenge()" do
+  describe "challenge" do
     it "will determine if a claim is true or false" do
       expect(g.challenge(dice: 19, value: 2)).to be false
     end
